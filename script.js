0dd78c10bf7819a5f2b294470fc48cd3a054fac3
Jeremi <jeremikibassapoaty@gmail.com>
2025-06-12 15:54:00 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const PU = "1000 fr"; // Prix unitaire
+document.getElementById('calculateButton').addEventListener('click',()=>{
+            alert('veillé entré une quantité ');
+            let quantity = prompt('quel est la quantité'); // Quantité
+            let tariftotal;
+            if (quantity <= 0) {
+                alert('veuillez entrer une quantité valide');
+            }
+
+        })
